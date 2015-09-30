bb05c4b21302b18656a210bd184dfbab535aa951
Luis Buenaventura <helloluis@me.com>
2015-09-30 11:13:12 +0800

    added validation

--- app/models/spree/event.rb
+++ app/models/spree/event.rb
@@ -3,6 +3,9 @@ class Spree::Event < ActiveRecord::Base
   scope :passed, ->(count=5) { where("starts_on <= :date", date: Date.today).limit(count) }
   default_scope { order('starts_on ASC') }
 
+  validates :title, presence: true
+  validates :starts_on, presence: true
+  
   acts_as_taggable
   
   def duplicate
